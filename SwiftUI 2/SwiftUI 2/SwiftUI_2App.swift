01155b06f1690f3f9d0a72d87cec4e4d50761ca6
WonGi-Kim <48573239+WonGi-Kim@users.noreply.github.com>
2021-03-10 19:21:11 +0900

    Add files via upload

---
+++ SwiftUI 2/SwiftUI 2/SwiftUI_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_2App.swift
+//  SwiftUI 2
+//
+//  Created by 김원기 on 2021/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
